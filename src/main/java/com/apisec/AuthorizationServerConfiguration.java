3b202ae1a88009330639e02180e6c98a8d9b94f9
lmaskisiki <lmaskisiki@users.noreply.github.com>
2017-10-30 07:13:11 +0200

    increased token validity seconds

--- src/main/java/com/apisec/AuthorizationServerConfiguration.java
+++ src/main/java/com/apisec/AuthorizationServerConfiguration.java
@@ -35,7 +35,7 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
 		clients.inMemory().withClient("client")
 				.authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
 				.authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT").scopes("read", "write", "trust").secret("secret")
-				.accessTokenValiditySeconds(60).refreshTokenValiditySeconds(180);
+				.accessTokenValiditySeconds(5000).refreshTokenValiditySeconds(1800);
 	}
 
 	@Override
